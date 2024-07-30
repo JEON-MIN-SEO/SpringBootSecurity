c1a29f04dbbafe1c76845312bf0355db3770df69
jmsparrowk <jmsparrowk@naver.com>
2024-07-30 17:36:05 +0900

    7/30

--- src/main/java/TestSecurity/demo/controller/MainController.java
+++ src/main/java/TestSecurity/demo/controller/MainController.java
@@ -1,10 +1,18 @@
 package TestSecurity.demo.controller;
 
+import TestSecurity.demo.dto.JoinDTO;
+import TestSecurity.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+
+    //생성자 주입으로 변경하기
+    @Autowired
+    private JoinService joinService;
 
     @GetMapping("/")
     public String mainP() {
@@ -14,5 +22,19 @@ public class MainController {
     @GetMapping("/login")
     public String loginP() {
         return "login";
+    }
+
+    @GetMapping("/join")
+    public String joinP() {
+        return "join";
+    }
+
+    @PostMapping("/signup")
+    public String signupP(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+
+        //회원가입 실패 시 어디로 연결?
+        return "redirect:/login";
     }
 }
---
+++ src/main/java/TestSecurity/service/JoinService.java
@@ -0,0 +1,12 @@
+package TestSecurity.service;
+
+import TestSecurity.demo.dto.JoinDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+    }
+}
